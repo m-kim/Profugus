9fc0dab2596e5a24fec47b4b1405557e9ae65a6c
Mark Kim <mbk@cs.utah.edu>
2016-10-06 09:40:12 -0400

    My x86 installer config.

--- install/cmake_x86_64.sh
+++ install/cmake_x86_64.sh
@@ -8,38 +8,38 @@ rm -rf CMakeCache.txt
 rm -rf CMakeFiles
 
 # SOURCE AND INSTALL
-SOURCE=<SET_SOURCE_DIR>
-INSTALL=<SET_INSTALL_DIR>
+SOURCE=/home/mkim/my-profugus
+INSTALL=/usr/local/Profugus
 
 # BUILD OPTIONS
 BUILD="DEBUG"
-MPI="ON"
+MPI="OFF"
 
 # TPL PATHS
-HDF5_PATH="/vendors/hdf5_parallel"
+HDF5_PATH="/usr/"
 MPI_PATH="/opt/openmpi/gcc/current"
 
 ##---------------------------------------------------------------------------##
 
 cmake \
+-G "CodeBlocks - Unix Makefiles" \
 -DCMAKE_BUILD_TYPE:STRING="$BUILD" \
 -DTPL_ENABLE_MPI:BOOL=$MPI \
 -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL \
 \
 -DMPI_BASE_DIR:PATH=$MPI_PATH \
 \
--DTPL_ENABLE_HDF5:BOOL=ON \
--DHDF5_INCLUDE_DIRS:PATH=$HDF5_PATH/include \
--DHDF5_LIBRARY_DIRS:PATH=$HDF5_PATH/lib \
+-DTPL_ENABLE_HDF5:BOOL="ON" \
+-DHDF5_INCLUDE_DIRS:PATH=/usr/include/hdf5/serial \
+-DHDF5_LIBRARY_DIRS:PATH=/usr/lib/x86_64-linux-gnu/hdf5/serial \
 \
--DBLAS_LIBRARY_DIRS:PATH=/vendors/gcc/atlas/lib \
--DLAPACK_LIBRARY_DIRS:PATH=/vendors/gcc/atlas/lib \
--DBLAS_LIBRARY_NAMES:STRING="f77blas;cblas;atlas" \
+-DBLAS_LIBRARY_DIRS:PATH=/usr/lib/libblas \
+-DLAPACK_LIBRARY_DIRS:PATH=/usr/lib \
+-DBLAS_LIBRARY_NAMES:STRING=";blas" \
 -DLAPACK_LIBRARY_NAMES:STRING="lapack" \
 \
 -DProfugus_CONFIGURE_OPTIONS_FILE:FILEPATH="${SOURCE}/install/base.cmake" \
 -DProfugus_ASSERT_MISSING_PACKAGES:BOOL=OFF \
-\
 ${SOURCE}
 
 ##---------------------------------------------------------------------------##
