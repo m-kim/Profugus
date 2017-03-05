6db5e5beacc32babe7afd59191258cd127fa71a7
Mark Kim <mbk@cs.utah.edu>
2017-03-05 15:57:44 -0500

    Stack works.

--- packages/MC/mc_driver/raycast/TreeIntersector.h
+++ packages/MC/mc_driver/raycast/TreeIntersector.h
@@ -211,120 +211,121 @@ void TreeIntersector::query(
   vec3 ret = box(rayOrigin, rayDir, lower, upper);
   if (ret[0] > 0){
 
-   recurse(points,
-           scalars,
-           rayOrigin,
-           rayDir,
-           fin_type,
-           face,
-           fin_offset,
-           fin_center,
-           minDistance,
-           hitIndex,
-           treePtr->child_cnt[_idx],
-           _idx);
-  }
-//   vtkm::Vec<vtkm::Float32,3> lower, upper;
-//   //vtkm::Id cur_offset = coords.GetNumberOfValues();
-//    vec3 ret;
-//   stack_ptr = 0;
-//   stack[stack_ptr] = _idx;
-//   s_cnt[stack_ptr] = _cnt;
-//   s_i[stack_ptr] = 0;
+//   recurse(points,
+//           scalars,
+//           rayOrigin,
+//           rayDir,
+//           fin_type,
+//           face,
+//           fin_offset,
+//           fin_center,
+//           minDistance,
+//           hitIndex,
+//           treePtr->child_cnt[_idx],
+//           _idx);
 
-//   stack_ptr++;
-//   int i = 0;
-//   do{
-//     for(;i < _cnt;){
-//       _vtx = treePtr->child_vtx[_idx + i];
-//       //lower = wtf.Get(_vtx);
-//       //upper = wtf.Get( _vtx + 1);
-//       vtkm::UInt8 type = ShapesPortal.Get(_vtx);
-//       vtkm::Id cur_offset = OffsetsPortal.Get(_vtx);
-//       if(type == BOUND){
-//         lower = points.Get(cur_offset);
-//         upper = points.Get(cur_offset+1);
-//         ret = box(rayOrigin, rayDir, lower, upper);
-//         if (ret[0] > 0){
-//           //push onto stack
-//           stack[stack_ptr] = _idx;
-//           s_cnt[stack_ptr] = _cnt;
-//           s_i[stack_ptr] = i;
+       vtkm::Vec<vtkm::Float32,3> lower, upper;
+       //vtkm::Id cur_offset = coords.GetNumberOfValues();
+        vec3 ret;
+        int _cnt = 0;
+       stack_ptr = 0;
+       stack[stack_ptr] = _idx;
+       _cnt = s_cnt[stack_ptr] = treePtr->child_cnt[_idx];
+       s_i[stack_ptr] = 0;
+       _idx = treePtr->child_idx[_idx];
 
-
-//           _cnt = treePtr->child_cnt[_idx + i];
-//           _idx = treePtr->child_idx[_idx + i];
-//           _vtx = treePtr->child_vtx[_idx];
-//           i = 0;
-//           stack_ptr++;
-//         }
-//         else{
-//           i++;
-//         }
-//       }
-//       else{
-
-//         //reached a leaf
-//         switch(type){
-//         case CYLINDER:
-//           cyl_bottom = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset));
-//           cyl_top = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset + 1));
-//           cyl_radius = vtkm::Float32(scalars.Get(cur_offset));
-//           //ret = vtkm::Vec<vtkm::Float32, 3>(1,1,1);
-//           ret = cylinder(rayOrigin, rayDir, cyl_bottom, cyl_top, cyl_radius);
-//           if (ret[0] > 0) {
-//             if (ret[1] < minDistance) {
-//               minDistance = ret[1];
-//               hitIndex = 35;
-//               fin_type = type;
-//               fin_offset = cur_offset;
-//             }
-//           }
-//           break;
-
-//         case BOX:
-//           box_ll = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset));
-//           box_ur = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset+1));
-//           ret = box(rayOrigin, rayDir, box_ll, box_ur);
-//           if (ret[0] > 0) {
-//             if (ret[1] < minDistance) {
-//               minDistance = ret[1];
-//               hitIndex = 35;
-//               fin_type = type;
-//               fin_offset = cur_offset;
-//               face = ret[2];
-//             }
-//           }
-//           break;
-
-//         case SPHERE:
-//           center = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset));//1.5;
-//           ret = sphere(rayOrigin, rayDir, center, vtkm::Float32(scalars.Get(cur_offset)));
-//           if (ret[0] > 0) {
-//             if (ret[1] < minDistance) {
-//               minDistance = ret[1];
-//               hitIndex = 35;
-//               fin_type = type;
-//               fin_offset = cur_offset;
-//               fin_center = center;
-//             }
-//           }
-//           break;
-//         }
-//         i++;
-//       }
-//     }
-//     if (i >= _cnt ){
-//       stack_ptr--;
-//       _idx = stack[stack_ptr];
-//       _cnt = s_cnt[stack_ptr];
-//       i = s_i[stack_ptr];
-//       i++;
-//     }
-//   }
-//   while(stack_ptr > 0);
+       stack_ptr++;
+       int i = 0;
+       do{
+         for(;i < _cnt;){
+           _vtx = treePtr->child_vtx[_idx + i];
+           //lower = wtf.Get(_vtx);
+           //upper = wtf.Get( _vtx + 1);
+           vtkm::UInt8 type = ShapesPortal.Get(_vtx);
+           vtkm::Id cur_offset = OffsetsPortal.Get(_vtx);
+           if(type == BOUND){
+             lower = points.Get(cur_offset);
+             upper = points.Get(cur_offset+1);
+             ret = box(rayOrigin, rayDir, lower, upper);
+             if (ret[0] > 0){
+               //push onto stack
+               stack[stack_ptr] = _idx;
+               s_cnt[stack_ptr] = _cnt;
+               s_i[stack_ptr] = i;
 
 
+               _cnt = treePtr->child_cnt[_idx + i];
+               _idx = treePtr->child_idx[_idx + i];
+               i = 0;
+               stack_ptr++;
+             }
+             else{
+               i++;
+             }
+           }
+           else{
+
+             //reached a leaf
+             switch(type){
+             case CYLINDER:
+               cyl_bottom = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset));
+               cyl_top = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset + 1));
+               cyl_radius = vtkm::Float32(scalars.Get(cur_offset));
+               //ret = vtkm::Vec<vtkm::Float32, 3>(1,1,1);
+               ret = cylinder(rayOrigin, rayDir, cyl_bottom, cyl_top, cyl_radius);
+               if (ret[0] > 0) {
+                 if (ret[1] < minDistance) {
+                   minDistance = ret[1];
+                   hitIndex = 35;
+                   fin_type = type;
+                   fin_offset = cur_offset;
+                 }
+               }
+               break;
+
+             case BOX:
+               box_ll = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset));
+               box_ur = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset+1));
+               ret = box(rayOrigin, rayDir, box_ll, box_ur);
+               if (ret[0] > 0) {
+                 if (ret[1] < minDistance) {
+                   minDistance = ret[1];
+                   hitIndex = 35;
+                   fin_type = type;
+                   fin_offset = cur_offset;
+                   face = ret[2];
+                 }
+               }
+               break;
+
+             case SPHERE:
+               center = vtkm::Vec<vtkm::Float32, 3>(points.Get(cur_offset));//1.5;
+               ret = sphere(rayOrigin, rayDir, center, vtkm::Float32(scalars.Get(cur_offset)));
+               if (ret[0] > 0) {
+                 if (ret[1] < minDistance) {
+                   minDistance = ret[1];
+                   hitIndex = 35;
+                   fin_type = type;
+                   fin_offset = cur_offset;
+                   fin_center = center;
+                 }
+               }
+               break;
+             }
+             i++;
+           }
+         }
+         if (i >= _cnt ){
+           stack_ptr--;
+           _idx = stack[stack_ptr];
+           _cnt = s_cnt[stack_ptr];
+           i = s_i[stack_ptr];
+           i++;
+         }
+       }
+       while(stack_ptr > 0);
+
+    }
 
 }
 
